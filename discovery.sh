a935a9a3fc4be61a38eeb0c56f4b8302b9541113
cloud <ashen-cloud@proton.me>
2024-06-23 05:29:01 +0100

    recon

---
+++ discovery.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+binaries=("dnsx", "httpx", "jq", "tew", "gospider", "puredns", "nmap")
+
+check_binary() {
+  if command -v "$1" >/dev/null 2>&1; then
+    echo "$1 is present"
+  else
+    echo "$1 is not present"
+    exit 1
+  fi
+}
+
+for binary in "${binaries[@]}"; do
+  check_binary "$binary"
+done
+
+echo "Starting scan:"
+
+HOST=$1
+
+# find subdomains
+echo $HOST | subfinder | anew subs.txt | wc -l
+shuffledns -d $HOST -mode bruteforce -w "~/SecLists/Discovery/DNS/bug-bounty-program-subdomains-trickest-inventory.txt" -r "~/resolvers.txt" | anew subs.txt | wc -l
+
+# resolve discovered ones
+puredns resolve "./subs.txt" -r "~/resolvers.txt" -w "./resolved.txt" | wc -l
+dnsx -l "./resolved.txt" -json -o "./dns.json" | jq -r '.a?[]?' | anew "./ips.txt" | wc -l
+
+# ports and servers
+nmap -T4 -vv -iL "./ips.txt" --top-ports 3000 -n --open -oX "./nmap.xml"
+tew -x "./nmap.xml" -dnsx "./dns.json" --vhost -o "./hostport.txt" | httpx -sr -srd "./responses" -json -o "./http.json"
+
+cat "./http.json" | jq -r '.url' | sed -e 's/:80$//g' -e 's/:443$//g' | sort -u > "./http.txt"
+
+# crawling
+gospider -S "./http.txt" --json | grep "{" | jq -r '.output?' | tee "./crawl.txt"
+
+# js
+cat "./crawl.txt" | grep "\.js" | httpx -sr -srd js
